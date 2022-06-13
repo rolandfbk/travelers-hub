6d80996ed3f1b5f74b7cb32d193f08762d13e1a8
Ogaga <iyaraogaga69@gmail.com>
2022-06-13 12:44:50 +0100

    Add styles to link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,20 @@ import logo from '../images/planet.png';
 
 const Navbar = () => (
 
-  <nav>
-    <img src={logo} alt="planet icon" className="header image" />
-    <h1 className="header-one">Space Travelers&apos; Hub</h1>
-    <Link className="navlink" to="/">Rockets</Link>
-    <Link className="navlink" to="/Missions">Missions</Link>
-    <Link className="navlink" to="/MyProfile">My Profile</Link>
-  </nav>
+  <header>
+    <nav>
+      <div className="logo">
+        <img src={logo} alt="planet icon" className="header-image" />
+        <h1 className="header-one">Space Travelers&apos; Hub</h1>
+      </div>
+      <div className="links">
+        <Link className="navlink" to="/">Rockets</Link>
+        <Link className="navlinks" to="/Missions">Missions</Link>
+        <Link className="navlink" to="/MyProfile">My Profile</Link>
+      </div>
+    </nav>
+    <div className="hr" />
+  </header>
 
 );
 
