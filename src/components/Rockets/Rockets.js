11d7d669fe5f76b8f852d0bbf6114d11fd4f43d1
Ogaga <iyaraogaga69@gmail.com>
2022-06-14 09:18:14 +0100

    style rocket page

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,6 +1,7 @@
 import { React, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchRocket, reserveRocket } from '../../Redux/Rocket/Rockets';
+import './Rockets.css';
 
 const Rockets = () => {
   const rocketData = useSelector((state) => state.rocketReducer);
@@ -28,14 +29,16 @@ const Rockets = () => {
   return (
     <div>
       {rocketData.map((rockets) => (
-        <div key={rockets.rocket_id}>
-          <div>
-            <img src={rockets.rocket_img} alt={rockets.rocket_name} />
-          </div>
-          <div>
-            <h1>{rockets.rocket_name}</h1>
-            <p>{rockets.rocket_description}</p>
-            <button type="button" onClick={rocketBooking}>Reserve Rocket</button>
+        <div key={rockets.rocket_id} className="display">
+          <div className="display-rocket">
+            <div>
+              <img className="rocket-image" src={rockets.rocket_img} alt={rockets.rocket_name} />
+            </div>
+            <div>
+              <h1 className="rocket-name">{rockets.rocket_name}</h1>
+              <p className="description">{rockets.rocket_description}</p>
+              <button type="button" onClick={rocketBooking}>Reserve Rocket</button>
+            </div>
           </div>
         </div>
       ))}
