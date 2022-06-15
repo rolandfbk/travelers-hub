834c0f30f479ef5d968ea72ccf17bd8bea276a01
rolandfbk <rolandfbk@gmail.com>
2022-06-15 10:44:13 +0200

    linters fixed

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <Router>
-          <App />
-      </Router>
-    </React.StrictMode>
-  </Provider>,
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
