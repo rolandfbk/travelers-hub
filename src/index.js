3b6a2822381d32602f7129ac3748e6a3d3e9b204
rolandfbk <rolandfbk@gmail.com>
2022-06-15 10:54:18 +0200

    linters fixed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
