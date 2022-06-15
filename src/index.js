256c55f14b1e90ac810be1153538f8f811071100
rolandfbk <rolandfbk@gmail.com>
2022-06-15 13:07:07 +0200

    linters fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import { Provider as Providers } from 'react-redux';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/configureStore';
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <Providers store={store}>
+      <Provider store={store}>
         <App />
-      </Providers>
+      </Provider>
     </Router>
   </React.StrictMode>,
 );
