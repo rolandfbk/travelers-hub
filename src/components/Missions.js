7f51e3af09fe1dfc62ad2177d6f6a95bff212229
rolandfbk <rolandfbk@gmail.com>
2022-06-15 14:15:44 +0200

    linters error fixed

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
-import { getMissionFromApi, memberLeave, memberJoin } from '../redux/mission/mission';
+import { getMissionFromApi, memberLeave, memberJoin } from '../Redux/mission/mission';
 import MissionList from './MissionList';
 
 const Missions = () => {
