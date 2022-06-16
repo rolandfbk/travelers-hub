9d270fff907d81238b55cc632003a920ebb25aef
rolandfbk <rolandfbk@gmail.com>
2022-06-16 12:39:00 +0200

    rocket.css updated

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -29,39 +29,39 @@ const Rockets = () => {
   return (
     <div className="rocket">
       <div className="rocket-align">
-      {rocketData.map((rockets) => (
-        <div key={rockets.rocket_id} className="display">
+        {rocketData.map((rockets) => (
+          <div key={rockets.rocket_id} className="display">
 
-          <img className="rocket-image" src={rockets.rocket_img} alt={rockets.rocket_name} />
+            <img className="rocket-image" src={rockets.rocket_img} alt={rockets.rocket_name} />
 
-          <div className="info">
-            <h1 className="rocket-name">{rockets.rocket_name}</h1>
-            <p className="description">
-              {rockets.reserved ? (<span className="span">Reserved</span>) : (false)}
-              {rockets.rocket_description}
-            </p>
-            {rockets.reserved ? (
-              <button
-                onClick={() => cancelBooking(rockets.rocket_id)}
-                type="button"
-                className="cancel-button"
-              >
-                Cancel Reservation
-              </button>
-            ) : (
-              <button
-                onClick={() => rocketBooking(rockets.rocket_id)}
-                type="button"
-                className="reserve-button"
-              >
-                Reserve Rocket
-              </button>
+            <div className="info">
+              <h1 className="rocket-name">{rockets.rocket_name}</h1>
+              <p className="description">
+                {rockets.reserved ? (<span className="span">Reserved</span>) : (false)}
+                {rockets.rocket_description}
+              </p>
+              {rockets.reserved ? (
+                <button
+                  onClick={() => cancelBooking(rockets.rocket_id)}
+                  type="button"
+                  className="cancel-button"
+                >
+                  Cancel Reservation
+                </button>
+              ) : (
+                <button
+                  onClick={() => rocketBooking(rockets.rocket_id)}
+                  type="button"
+                  className="reserve-button"
+                >
+                  Reserve Rocket
+                </button>
 
-            )}
+              )}
+            </div>
+
           </div>
-
-        </div>
-      ))}
+        ))}
       </div>
     </div>
   );
